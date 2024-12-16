349be510286c72ec197caaad22f4b450ddd5fd44
Bar-Levi <bar314levi@gmail.com>
2024-12-16 09:31:48 +0200

    Setup frontend with React

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to CareerAgent</h1>
+      <p>Your multi-platform application is up and running!</p>
+    </div>
+  );
+}
+
+export default App;
