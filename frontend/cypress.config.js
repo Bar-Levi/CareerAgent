571575d7907e624a6ac69020498b18f859f9cc12
Rony Bubnovsky <rony.bubnovsky@gmail.com>
2024-12-29 17:08:56 +0200

    NOW CI WILL WORK

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,7 +3,7 @@ require('dotenv').config(); // Load variables from .env
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: `http://localhost:${process.env.FRONTEND_PORT}`,
+    baseUrl: `http://localhost:${process.env.REACT_APP_FRONTEND_PORT}`,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
