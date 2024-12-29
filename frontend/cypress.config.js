2a9dca288303920b022c3aa95f21c2ec6353daa0
Rony Bubnovsky <rony.bubnovsky@gmail.com>
2024-12-29 02:30:56 +0200

    cypress integration into CI

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,7 +3,7 @@ require('dotenv').config(); // Load variables from .env
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: `http://localhost:${process.env.PORT}`,
+    baseUrl: `http://localhost:${process.env.FRONTEND_PORT}`,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
