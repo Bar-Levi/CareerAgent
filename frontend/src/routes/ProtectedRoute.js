302fddc7fcad980ae344cf3dd478c06e1e9711e3
Bar-Levi <bar314levi@gmail.com>
2025-01-16 22:01:31 +0200

    Fixed authentication logic of getting token

--- frontend/src/routes/ProtectedRoute.js
+++ frontend/src/routes/ProtectedRoute.js
@@ -5,7 +5,8 @@ import { useLocation } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
     const { state } = useLocation();
-    if (!isAuthenticated() || !state) {
+    console.log("state: " + JSON.stringify(state));
+    if (!isAuthenticated(state.token) || !state) {
         return <Navigate to="/" replace />;
     }
     return children;
